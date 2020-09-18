86e69607ac97c24842ca7b12e17a7f97224559ac
Josh Lamb <joshlamb@gmail.com>
2020-09-17 19:51:05 -0700

    fresh CRA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React, { useState, useEffect } from "react"
-import socketIOClient from "socket.io-client"
-const urlRoot = process.env.NODE_ENV === "production" ? "https://horsefeathers.herokuapp.com" : "http://127.0.0.1:4200"
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [response, setResponse] = useState("")
-
-  useEffect(() => {
-    const socket = socketIOClient(urlRoot)
-    socket.on("FromAPI", data => {
-      setResponse(data);
-    });
-
-    return () => socket.disconnect()
-  }, [])
-
   return (
-    <p>
-      It's <time dateTime={response}>{response}</time>
-    </p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
